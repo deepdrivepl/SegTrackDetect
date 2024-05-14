20e970c826ec4d6c787a66fb97fa3b655d17f9e8
koseq <aleksandraaa.kos@gmail.com>
2024-05-14 21:05:21 +0200

    rm legacy code

--- download_models.sh
+++
@@ -1,5 +0,0 @@
-mkdir trained_models
-
-wget https://www.dropbox.com/s/m1fl7elr4k17utl/u2netp-00000104.pth?dl=1 -O trained_models/u2netp.pth
-wget https://www.dropbox.com/s/sv7db7qa0n4t53l/mtsd001_best.weights?dl=1 -O trained_models/mtsd001_best.weights
-wget https://www.dropbox.com/s/w33xeyxgs80ckqy/mtsd001-960.cfg?dl=1 -O trained_models/mtsd001-960.cfg
